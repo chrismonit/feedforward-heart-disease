9cf1627d58bd76de3515c8d85d6573cd7972193d
Chris Monit <c.monit.12@ucl.ac.uk>
2020-05-06 14:03:43 +0100

    implemented binary logistic regression class, gives same results as previous implementation

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 import src.preproc as preproc
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))  # assuming this file in <proj_root>/src
@@ -16,8 +17,54 @@ def grad_sigmoid(z):
     return sigmoid(z) * (1 - sigmoid(z))
 
 
-# def cost(y_pred, y_true):
-#     return -1./len(y_true) * np.sum(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+# TODO regularisation? Class balancing?
+class Log_reg():
+    def __init__(self, num_features, w_init=None, b_init=None, predict_thresh=0.5):
+        self.w = w_init if w_init else np.zeros((num_features, 1))
+        self.b = b_init if b_init else 0
+        self.predict_thresh = predict_thresh
+
+    def _forward(self, X, y):
+        Z = (np.dot(self.w.T, X) + self.b)
+        A = self.sigmoid(Z)
+        cost = self.cross_entropy_cost(A, y)
+        return A, cost
+
+    @staticmethod
+    def _backward(A, X, y):
+        dz = A - y.values
+        dw = np.dot(X, dz.T) / len(y)  # dz = A - Y
+        db = np.sum(dz) / len(y)
+        return dw, db
+
+    def _update(self, dw, db, learn_rate):
+        self.w = self.w - learn_rate * dw
+        self.b = self.b - learn_rate * db
+
+    # TODO could return cost values during fitting. Best as a generator??
+    def fit(self, X, y, learn_rate, num_iterations, print_frequency_fraction=0.1):
+        for i in range(num_iterations):
+            A, cost = self._forward(X, y)
+            dw, db = self._backward(A, X, y)
+            self._update(dw, db, learn_rate)
+            if i % int(1./print_frequency_fraction) == 0:
+                print(f"LogReg: iteration={i}, cost={cost}")
+
+    @staticmethod
+    def cross_entropy_cost(y_pred, y_true):
+        cost = np.sum(y_true.values * np.log(y_pred) + (1. - y_true.values) * np.log(1. - y_pred)) / -len(y_true)
+        return cost.squeeze()
+
+    @staticmethod
+    def sigmoid(z):
+        return 1. / (1 + np.exp(-z))
+
+    def predict(self, X):
+        Z = np.dot(self.w.T, X) + self.b
+        A = sigmoid(Z)
+        y_pred = [int(a > self.predict_thresh) for a in A.squeeze()]
+        return y_pred
+        # print("accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 
 def main():
@@ -25,8 +72,8 @@ def main():
     data = preproc.preproc_file(os.path.join(DATA_DIR, "short_names_capsid_master.fasta"), 'fasta')
     data = data[data["label"].isin(["B", "C"])]
     data["label"] = data["label"].map({'B': 1, 'C': 0})
-    b_sample = data[data["label"] == 1].iloc[:10, :]
-    c_sample = data[data["label"] == 0].iloc[:10, :]
+    b_sample = data[data["label"] == 1].iloc[:500, :]
+    c_sample = data[data["label"] == 0].iloc[:500, :]
     sample = pd.concat([b_sample, c_sample], axis=0)
 
     labels = sample['label']
@@ -45,19 +92,28 @@ def main():
     y = y_train
 
     n_features, m = X.shape
-    n_iterations = 500
+    n_iterations = 100
+    alpha = 0.01
     print(f"m={m}, n_features={n_features}", f"", "", sep="\n")
+
+    model = Log_reg(num_features=n_features)
+    # A, cost = model._forward(X, y)
+    # print(f"From model, cost={cost}, A={A}")
+    model.fit(X, y, alpha, num_iterations=n_iterations, print_frequency_fraction=0.1)
+    model_y_pred = model.predict(X_test)
+    print(f"Model accuracy: {metrics.accuracy_score(y_test, model_y_pred)}")
+    print()
+    # exit()
 
     # w_init_scale = 0.01
     # wT = np.random.randn(1, n_features) * w_init_scale
     w = np.zeros((n_features, 1))  # can initilise with 0 for LR
     b = 0.0
-    print("Input dimensions:"
-          f"X={X.shape}",
-          f"y={y.shape}",
-          f"w={w.shape}",
-          f"", "", sep="\n")
-    alpha = 0.01
+    # print("Input dimensions:"
+    #       f"X={X.shape}",
+    #       f"y={y.shape}",
+    #       f"w={w.shape}",
+    #       f"", "", sep="\n")
     for i_iter in range(n_iterations):
         Z = (np.dot(w.T, X) + b)
         A = sigmoid(Z)
@@ -82,8 +138,9 @@ def main():
     Z_test = np.dot(w.T, X_test) + b
     A_test = sigmoid(Z_test)
     y_pred = [int(a > THRESH) for a in A_test.squeeze()]
-    print(y_pred)
-    print(y_test)
+    # print(y_pred)
+    # print(y_test)
+    print("accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 
 
